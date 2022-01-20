eb69e252543ea6fdd87a0f246099c3802474094f
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-01-20 19:48:47 +0300

    NOT WORKS in progress

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,8 +1,6 @@
 """Page loader main module."""
 import os
 
-import requests
-from bs4.element import Tag
 from page_loader.constants import ATTRIBUTES, DIR_SUFFIX, SLASH
 from page_loader.io import create_dir, write_to_file
 from page_loader.log import logger
@@ -12,42 +10,42 @@ from page_loader.names import (
     make_name,
     make_paths,
 )
-from page_loader.web import get_from_url, make_soup
+from page_loader.web import get_from_url, prepare
 from progress.bar import ChargingBar
 
 
-def _get_resource(file_path: str, tag: Tag, base_url, attr):
-    try:
-        res_path = tag[attr]
-    except KeyError:
-        return None
-    res_url = generate_url(res_path, base_url)
-    if res_url is None:
-        return None
-    try:
-        res_content = get_from_url(res_url)
-    except requests.RequestException as err:
-        logger.info(err)
-        logger.warning(f"Can't download from {res_url}")
-        return None
-    res_path = make_file_name(res_path, tag, base_url)
-    full_path = os.path.join(res_path, file_path, res_path)
-    new_src = os.path.join(make_name(base_url, DIR_SUFFIX), res_path)
-    tag[attr] = new_src
-    return res_content, full_path
+# def _get_resource(file_path: str, tag: Tag, base_url, attr):
+#     try:
+#         res_path = tag[attr]
+#     except KeyError:
+#         return None
+#     res_url = generate_url(res_path, base_url)
+#     if res_url is None:
+#         return None
+#     try:
+#         res_content = get_from_url(res_url)
+#     except requests.RequestException as err:
+#         logger.info(err)
+#         logger.warning(f"Can't download from {res_url}")
+#         return None
+#     res_path = make_file_name(res_path, tag, base_url)
+#     full_path = os.path.join(res_path, file_path, res_path)
+#     new_src = os.path.join(make_name(base_url, DIR_SUFFIX), res_path)
+#     tag[attr] = new_src
+#     return res_content, full_path
 
 
-def _get_tags(tags, files_dir_path: str, url: str):
-    progress_bar = ChargingBar(message=f"Loading {url}", max=len(tags))
-    tags_content = []
-    for tag in tags:
-        attr_name = ATTRIBUTES[tag.name]
-        payload = _get_resource(files_dir_path, tag, url, attr_name)
-        if payload is not None:
-            tags_content.append(payload)
-        progress_bar.next()
-    progress_bar.finish()
-    return tags_content
+# def _get_tags(tags, files_dir_path: str, url: str):
+#     progress_bar = ChargingBar(message=f"Loading {url}", max=len(tags))
+#     tags_content = []
+#     for tag in tags:
+#         attr_name = ATTRIBUTES[tag.name]
+#         payload = _get_resource(files_dir_path, tag, url, attr_name)
+#         if payload is not None:
+#             tags_content.append(payload)
+#         progress_bar.next()
+#     progress_bar.finish()
+#     return tags_content
 
 
 def download(url: str, output: str = "current") -> str:
@@ -61,20 +59,14 @@ def download(url: str, output: str = "current") -> str:
         str: Path to saved file.
     """
     files_dir_path, output_html_path = make_paths(output, url.strip(SLASH))
+    html_file, resources = prepare(url.strip(SLASH), files_dir_path)
+    write_to_file(output_html_path, html_file)
 
-    soup = make_soup(url.strip(SLASH))
-
-    tags_content = _get_tags(
-        soup.find_all(["img", "link", "script"]),
-        files_dir_path,
-        url.strip(SLASH),
-    )
-
-    if tags_content:
+    if resources:
         create_dir(files_dir_path)
-        for tag in tags_content:
-            content_to_write, full_path = tag
-            write_to_file(full_path, content_to_write)
 
-    write_to_file(output_html_path, soup.prettify())
+        for res_url, file_path in resources:
+            file_content = get_from_url(res_url)
+            write_to_file(file_path, file_content)
+
     return output_html_path
--- page_loader/web.py
+++ page_loader/web.py
@@ -1,24 +1,11 @@
 """Web module."""
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 from page_loader.log import logger
 
 
-def _make_soup(url: str) -> BeautifulSoup:
-    """Do make BeautifulSoup from URL.
-
-    Args:
-        url (str): Web page URL
-
-    Returns:
-        BeautifulSoup: BeautifulSoup from URL
-    """
-    raw_html = _get_from_url(url)
-    logger.info('BeautifulSoup was created')
-    return BeautifulSoup(raw_html, 'html.parser')
-
-
-def _get_from_url(url: str) -> requests.Response:
+def get_from_url(url: str) -> requests.Response:
     """Do download any resource from URL.
 
     Args:
@@ -32,7 +19,36 @@ def _get_from_url(url: str) -> requests.Response:
     logger.info(f'Resource from {url} was downloaded')
     return response.content
 
-def prepare(url: str, files_dir_path: str):
+
+def _make_soup(url: str) -> BeautifulSoup:
+    raw_html = get_from_url(url)
+    return BeautifulSoup(raw_html, 'html.parser')
+
+def _parse_tag(tag: Tag, files_dir_path: str) -> tuple[str]:
+    pass
+
+def _write_new_src(tag: Tag, new_src: str):
+    pass
+
+def prepare(url: str, files_dir_path: str) -> tuple[str, tuple]:
+    """Do download HTML file, find all tags with resources, changes resource tags with local sources.
+
+    Args:
+        url (str): URL to download
+        files_dir_path (str): Path to local resources
+
+    Returns:
+        tuple[str, tuple]: Modified HTML file, tuple with resources URLs and pathes to save
+    """
+    soup = _make_soup(url)
     resources = []
-    html_file = ""
+    res_tags = soup.find_all(["img", "link", "script"])
+    for tag in res_tags:
+        res_url, file_path, new_src = _parse_tag(tag, files_dir_path)
+        _write_new_src(tag, new_src)
+        resources.append(res_url, file_path)
+
+
+    html_file = soup.prettify()
     return html_file, resources
+
